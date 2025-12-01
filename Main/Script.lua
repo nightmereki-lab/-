d5462a8f0a4d2a769c85f3a162f9fee779b7833f
nightmereki-lab <nightmereki@gmail.com>
2025-12-01 19:33:21 -0300

    Update Script.lua

--- Main/Script.lua
+++ Main/Script.lua
@@ -1,1 +1,5 @@
+local targetPlace = 121864768012064
 
+if game.PlaceId == targetPlace then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nightmereki-lab/-/refs/heads/main/Games/Fisch%20it/Script.lua"))()
+end
